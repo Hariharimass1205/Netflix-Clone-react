db8fc1f055b2f918cc63a3ac5dfde5d4c242a393
Hariharan <hariharimass1205@gmail.com>
2024-07-23 13:44:51 +0530

    ,j,g

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
         <Route path= '/player/:id' element={<Player/>} />
       </Routes>
     </Router>
